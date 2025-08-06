45899f3e55934ee5774f872ca8293620742ae4ea
taetae-0304 <taewoo-0304@naver.com>
2025-08-07 00:13:24 +0900

    4륜에서 L298N을 사용하는 후륜으로 수정. 초음파 센서를 추가하여 낙하방지.벽과 바닥의 거리를 측정하여 부딪히거나 떨어지지 않게

--- car.ino
+++ car.ino
@@ -1,36 +1,81 @@
-const int TRIG = 9;
-const int ECHO = 8;
-const int m1 = 7;
-const int m2 = 6;
-const int m3 = 5;
-const int m4 = 4;
+const int TRIG1 = 9;
+const int ECHO1 = 8;
+const int IN1 = 7; //후륜으로 바퀴 2개 이용 & L298N 사용하여 제어
+const int IN2 = 6;
+const int ENA = 5;
+const int TRIG2 = 4;
+const int ECHO2 = 3;
 
 void setup() {
   Serial.begin(9600); //통신속도
-  pinMode(TRIG,OUTPUT);
-  pinMode(ECHO,INPUT);
-  pinMode(m1,OUTPUT);
-  pinMode(m2,OUTPUT);
-  pinMode(m3,OUTPUT);
-  pinMode(m4,OUTPUT);
+  pinMode(TRIG1,OUTPUT);
+  pinMode(ECHO1,INPUT);
+  pinMode(TRIG2,OUTPUT);
+  pinMode(ECHO2,INPUT);
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(ENA,OUTPUT);
 }
 
 void loop() {
- long duration, distance;   //long == int라 생각 대신 저장 공간이 더 많은
- digitalWrite(TRIG, LOW); 
+ long duration1, distance1;   //long == int라 생각 대신 저장 공간이 더 많은
+ digitalWrite(TRIG1, LOW); 
  delayMicroseconds(2); 
- digitalWrite(TRIG, HIGH); 
+ digitalWrite(TRIG1, HIGH); 
  delayMicroseconds(10); 
- digitalWrite(TRIG, LOW); 
- duration = pulseIn (ECHO, HIGH);
+ digitalWrite(TRIG1, LOW); 
+ duration1 = pulseIn (ECHO1, HIGH);
   
- distance = duration * 17 / 1000; //초음파가 이동한 거리를 cm으로 변환하는 공식
+ distance1 = duration1 * 17 / 1000; //초음파가 이동한 거리를 cm으로 변환하는 공식
 
 
- Serial.println(duration ); //시리얼 모니터에 표시하는 것
- Serial.print("\nDIstance : "); 
- Serial.print(distance);  
+ Serial.println(duration1 ); //시리얼 모니터에 표시하는 것
+ Serial.print("\nDIstance1 : "); 
+ Serial.print(distance1);  
  Serial.println(" Cm"); 
  delay(1000); 
+
+ long duration2, distance2;   
+ digitalWrite(TRIG2, LOW); 
+ delayMicroseconds(2); 
+ digitalWrite(TRIG2, HIGH); 
+ delayMicroseconds(10); 
+ digitalWrite(TRIG2, LOW); 
+ duration2 = pulseIn (ECHO2, HIGH);
+  
+ distance2 = duration2 * 17 / 1000; 
+
+
+ Serial.println(duration2 ); 
+ Serial.print("\nDIstance2 : "); 
+ Serial.print(distance2);  
+ Serial.println(" Cm"); 
+ delay(1000); 
+
+
+ if(distance1 < 5) //벽과의 거리를
+ {
+  digitalWrite(IN1, LOW); 
+ digitalWrite(IN2, HIGH);
+ analogWrite(ENA, 200);
+ }else {
+  // 5cm 이상일 때
+  digitalWrite(IN1, HIGH); //L298N 사용해서 정방향 이동 속도는 200으로 한 것
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 200);
+}
+
+if(distance2 > 5) //바닥과의 거리를
+ {
+  digitalWrite(IN1, LOW); 
+ digitalWrite(IN2, HIGH);
+ analogWrite(ENA, 200);
+ }else {
+  // 5cm 이하일 때
+  digitalWrite(IN1, HIGH); //L298N 사용해서 정방향 이동 속도는 200으로 한 것
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 200);
+}
+
 
 }
